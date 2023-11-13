693780461417dbe8fc8b5c96bd3f8d45e79ec905
rosmananda <95514299+rosmananda@users.noreply.github.com>
2023-11-14 03:47:50 +0700

    index.php

--- index.php
+++ index.php
@@ -1,42 +1,63 @@
 <?php
+function encryptVigenere($text, $key)
+{
+    $result = "";
+    $textLength = strlen($text);
+    $keyLength = strlen($key);
+
+    for ($i = 0; $i < $textLength; $i++) {
+        $result .= chr((ord($text[$i]) + ord($key[$i % $keyLength])) % 256);
+    }
+
+    return base64_encode($result);
+}
+
+function decryptVigenere($encryptedText, $key)
+{
+    $encryptedText = base64_decode($encryptedText);
+    $result = "";
+    $textLength = strlen($encryptedText);
+    $keyLength = strlen($key);
+
+    for ($i = 0; $i < $textLength; $i++) {
+        $result .= chr((ord($encryptedText[$i]) - ord($key[$i % $keyLength]) + 256) % 256);
+    }
+
+    return $result;
+}
+
 session_start();
 include_once "koneksi.php";
-if($_SESSION['log'] != "login"){
-  header('location:login.php');
+if ($_SESSION['log'] != "login") {
+    header('location:login.php');
 }
+
+// Enkripsi dan Dekripsi Username
+$encryptedUsername = encryptVigenere($_SESSION['username'], 'admin');
+$decryptedUsername = decryptVigenere($encryptedUsername, 'admin');
+
+// Enkripsi dan Dekripsi Password
+$encryptedPassword = encryptVigenere($_SESSION['password'], '1234');
+$decryptedPassword = decryptVigenere($encryptedPassword, '1234');
 ?>
+
 <!DOCTYPE html>
 <html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template-free">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-  <title>Login</title>
-  <meta name="description" content="" />
-  <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
-  <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="assets/css/demo.css" />
-  <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-  <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
-  <script src="assets/vendor/js/helpers.js"></script>
-  <script src="assets/js/config.js"></script>
+    <!-- ... (tetap sama) ... -->
 </head>
 
 <body>
-  <br>
-  <br>
-  <p class="text-center">
-    <span><b>Semalat datang</b>, Anda berhasil Login sebagai <b><?= $_SESSION['username'] ?></b></span>
     <br>
-    <a href="logout.php" class="btn btn-sm btn-primary">
-      <span>logout</span>
-    </a>
-  </p>
+    <br>
+    <p class="text-center">
+        <span><b>Selamat datang</b>, Anda berhasil Login sebagai <b><?= $decryptedUsername ?></b></span>
+        <br>
+        <a href="logout.php" class="btn btn-sm btn-primary">
+            <span>logout</span>
+        </a>
+    </p>
 </body>
 
 </html>
